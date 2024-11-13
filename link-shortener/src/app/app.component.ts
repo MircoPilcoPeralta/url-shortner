561f7ed2d0ed04c90241f4e2c852f55f3e3a4a63
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2024-11-13 16:39:24 -0400

    add page presentation

--- link-shortener/src/app/app.component.ts
+++ link-shortener/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
